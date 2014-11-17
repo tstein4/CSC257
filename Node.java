f921f4e22f61d2ec8203446ceaeef911dc5b90a1
TJ <tj@steinspace.com>
2014-11-17 02:00:03 -0500

    Finished (I think?)

--- Node.java
+++ Node.java
@@ -1,22 +1,30 @@
 import java.util.ArrayList;
+import java.util.Random;
 import java.io.*;
 import java.net.*;
 
 public class Node{
-	String dir;
-	String hostname;
+	File[] files;
 	int port;
+	String hostAddress;
 	ArrayList<String> neighborIPs;
-	int[] searchedFiles;
+	ArrayList<Integer> searchIDs;
 	ServerThread sThread;
 
-	public Node(int port, String dir, ArrayList<String> neighborIPs){
-		this.port = port;
-		this.dir = dir;
-		searchedFiles = new int[10];
+	public Node(int port, File[] files, ArrayList<String> neighborIPs){
+		this.files = files;
+		searchIDs = new ArrayList<Integer>();
 		this.neighborIPs = neighborIPs;
+		this.port = port;
+		try{
+			hostAddress = InetAddress.getLocalHost().getHostAddress();
+		}
+		catch(IOException e){
+			hostAddress = "127.0.0.1";
+			System.out.println("Error: can't find host address");
+		}
 		for (String ip : neighborIPs){
-			sendToNeighbor(ip, "addneighbor|"+port);
+			sendToIP(ip, "add|"+hostAddress+":"+port);
 		}
 	}
 
@@ -29,17 +37,16 @@ public class Node{
 		sThread.terminate();
 	}
 
-	public void worker(){
-
-	}
-
-	public void startSeach(){
-
+	public void quit(){
+		stopServer();
+		for (String ip: neighborIPs){
+			sendToIP(ip, "remove|"+hostAddress+":"+port);
+		}
 	}
 
 	public void pingNeighbors(){
 		for (String ip : neighborIPs){
-			sendToNeighbor(ip, "ping");
+			sendToIP(ip, "ping");
 		}
 	}
 
@@ -49,10 +56,54 @@ public class Node{
 		}
 	}
 
-	public void sendToNeighbor(String ip, String message){
+	public void printFiles(){
+		for (File file : files){
+			System.out.println(file.getName());
+		}
+	}
+
+	public String getIP(){
+		return hostAddress+":"+port;
+	}
+
+	public int searchFiles(String filePrefix){
+		for (int i = 0; i < files.length; i++){
+			if (files[i].getName().indexOf(filePrefix) == 0){
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public void search(String filePrefix, int ttl, int id, String sourceIP){
+		if (searchIDs.contains(id)){
+			return;
+		}
+		searchIDs.add(id);
+		int newTTL = ttl-1;
+		System.out.println("\nSearching for files with prefix: " + filePrefix);
+		System.out.println("Query ID: " + id);
+		System.out.println("TTL Value: " + ttl);
+		int index = searchFiles(filePrefix);
+		if (index != -1){
+			System.out.println("\n* Found file " + files[index].getName());
+			sendToIP(sourceIP, "found|"+files[index].getName()+"|"+hostAddress+":"+port);
+		}
+		else{
+			System.out.println("\nFile not found.");
+		}
+		if (newTTL >= 0){
+			System.out.println("\nTTL is greater than 0, propegating search.");
+			for (String ip : neighborIPs){
+				sendToIP(ip, "search|"+filePrefix+"|"+newTTL+"|"+id+"|"+sourceIP);
+			}
+		}
+		System.out.print("\n$ ");
+	}
+
+	public void sendToIP(String ip, String message){
 		String hostname = ip.substring(0, ip.indexOf(":"));
 		int portNumber = Integer.parseInt(ip.substring(ip.indexOf(":")+1));
-
 		try (
 			Socket sock = new Socket(hostname, portNumber);
 			PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
@@ -62,53 +113,8 @@ public class Node{
 			System.out.println("UnknownHostException");
 			System.exit(1);
 		} catch (IOException e){
-			System.out.println(ip);
 			e.printStackTrace();
 			System.exit(1);
-		}
-	}
-
-	public static void main(String[] args) throws IOException{
-		if (args.length < 2){
-			System.out.println("Usage: java Node <port> <file_directory> <list_of_node_ips>");
-			System.exit(0);
-		}
-		ArrayList<String> ipList = new ArrayList<String>();
-		if (args.length > 2){
-			for (int i = 2; i < args.length; i++){
-				if (args[i].indexOf(':') < 0)
-					ipList.add(args[i]+":8765");
-				else
-					ipList.add(args[i]);
-			}
-		}
-		int port=8765;
-		try{
-			port = Integer.parseInt(args[0]);
-		}
-		catch (NumberFormatException e){
-			System.out.println("Error: port number not parseable.");
-			System.exit(0);
-		}
-		Node node = new Node(port, args[1], ipList);
-		node.startServer();
-
-		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
-		String userInput;
-		for(;;){
-			userInput = stdIn.readLine();
-			if (userInput.equals("quit")){
-				System.out.println("Quitting.");
-				System.exit(0);
-			}
-			else if (userInput.equals("print")){
-				node.printNeighbors();
-			}
-			else if (userInput.equals("ping")){
-				node.pingNeighbors();
-			}
-			else
-				System.out.println(userInput);
 		}
 	}
 
@@ -121,27 +127,59 @@ public class Node{
 
 		public void run(){
 			try(
-				// PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			) {
-				// System.out.println("ip: " +socket.getInetAddress().toString().substring(1));
-				// System.out.println("port: " +socket.getPort());
 				String inputLine, outputLine;
 				while ((inputLine = in.readLine()) != null){
-					if (inputLine.indexOf("addneighbor") >= 0){
-						String port = inputLine.substring(inputLine.indexOf("|")+1);
-						if (!neighborIPs.contains(socket.getInetAddress().toString().substring(1)+":"+port)){
-							neighborIPs.add(socket.getInetAddress().toString().substring(1)+":"+port);
+					if (inputLine.indexOf("add|") == 0){
+						// Add syntax: add|<neighbor ip>
+						String addr = inputLine.substring(inputLine.indexOf("|")+1);
+						if (!neighborIPs.contains(addr)){
+							System.out.println("\nAdding new neighbor at " + addr+"\n");
+							System.out.print("$ ");
+							neighborIPs.add(addr);
 						}
 					}
-					else if(inputLine.indexOf("ping") >= 0){
-						System.out.println("Got pinged by " + socket.getInetAddress().toString().substring(1));
+					else if (inputLine.indexOf("remove|")==0){
+						// Remove syntax: remove|<neighbor ip>
+						String addr=inputLine.substring(inputLine.indexOf("|")+1);
+						if (neighborIPs.contains(addr)){
+							System.out.println("\nRemoving neighbor at " + addr+"\n");
+							System.out.print("$ ");
+							neighborIPs.remove(addr);
+						}
 					}
-					// System.out.println(inputLine);
+					else if(inputLine.equals("ping")){
+						System.out.println("\nGot pinged by " + socket.getInetAddress().toString().substring(1)+"\n");
+						System.out.print("$ ");
+					}
+					else if (inputLine.indexOf("found|") == 0){
+						// Found syntax: found|<filename>|<finding ip>
+						String[] vals = inputLine.split("\\|");
+						System.out.println("\n* Found file " + vals[1] + " at IP " + vals[2] + "\n");
+						System.out.print("$ ");
+					}
+					else if (inputLine.indexOf("search|") == 0){
+						// Search query syntax: search|<prefix>|<ttl>|<id>|<source ip>
+						sleep(getRandomSleepTime()); // This is to prevent a node with 2 equal paths from reporting twice.
+						String[] vals = inputLine.split("\\|");
+						String prefix = vals[1];
+						int ttl = Integer.parseInt(vals[2]);
+						int id = Integer.parseInt(vals[3]);
+						String sourceIP = vals[4];
+						search(prefix, ttl, id, sourceIP);
+					}
 				}
 			} catch (IOException e){
 				e.printStackTrace();
+			} catch (InterruptedException e){
+				System.out.println("Interrupted.");
 			}
+		}
+
+		public int getRandomSleepTime(){
+			Random rand = new Random();
+			return rand.nextInt(1000);
 		}
 	}
 
@@ -155,11 +193,11 @@ public class Node{
 		}
 
 		public void run(){
-			try(
+			try (
 				ServerSocket serverSocket = new ServerSocket(portNumber);
-			){
+			)
+			{
 				while (running){
-					// System.out.println("pls");
 					new WorkerThread(serverSocket.accept()).start();
 				}
 			} catch(IOException e){
---
+++ PTP.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+
+public class PTP{
+	final static int TTL = 2;
+
+	public static void main(String[] args) throws IOException{
+		if (args.length < 2){
+			System.out.println("Usage: java Node <port> <file_directory> <list_of_node_ips>");
+			System.exit(0);
+		}
+		ArrayList<String> ipList = new ArrayList<String>();
+		if (args.length > 2){
+			for (int i = 2; i < args.length; i++){
+				if (args[i].indexOf(':') < 0)
+					ipList.add(args[i]+":8765");
+				else
+					ipList.add(args[i]);
+			}
+		}
+		int port=8765;
+		try{
+			port = Integer.parseInt(args[0]);
+		}
+		catch (NumberFormatException e){
+			System.out.println("Error: port number not parseable.");
+			System.exit(1);
+		}
+		File dir = new File(args[1]);
+		File[] files = new File[0];
+		if (dir.isDirectory()){
+			files = dir.listFiles();
+		}
+		else{
+			System.out.println("Error: not a valid directory.");
+			System.exit(1);
+		}
+		Node node = new Node(port, files, ipList);
+		node.startServer();
+
+		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+		String userInput;
+		System.out.print("$ ");
+		for(;;){
+			userInput = stdIn.readLine();
+			if (userInput.equals("quit")){
+				System.out.println("Quitting.");
+				node.quit();
+				System.exit(0);
+			}
+			else if (userInput.equals("print neighbors")){
+				node.printNeighbors();
+				System.out.print("\n$ ");
+			}
+			else if (userInput.equals("print files")){
+				node.printFiles();
+				System.out.print("\n$ ");
+			}
+			else if (userInput.equals("ping")){
+				node.pingNeighbors();
+				System.out.print("\n$ ");
+			}
+			else if (userInput.indexOf("find ") == 0){
+				String prefix = userInput.substring(userInput.indexOf(" ") + 1);
+				int id = getRandomInt();
+				node.search(prefix, TTL, id, node.getIP());
+			}
+			else if (userInput.equals("help") || userInput.equals("?")){
+				System.out.println("Welcome to the help output.");
+				System.out.println("Commdands:");
+				System.out.println("quit            - This closes the node, and informs all neighbors that the node is no longer in the network.");
+				System.out.println("print neighbors - This prints the IP Addresses and ports of the node's neighbors");
+				System.out.println("print files     - This prints the files in the node's directory.");
+				System.out.println("ping            - This sends a message to all neighbors and prints a \"ping\" in their output.");
+				System.out.println("find <prefix>   - This command initiates the Gnutella flooding peer-to-peer search.");
+				System.out.println("                  All nodes print their progress, and include TTL value, Query ID, and file prefix.");
+				System.out.println("                  Hits are reported both in the finding node and the initial node.");
+				System.out.print("\n$ ");
+			}
+		}
+	}
+	public static int getRandomInt(){
+		Random rand = new Random();
+		return rand.nextInt(100000);
+	}
+}
